39363b179d0c395f672e2d9c10150c16b64ad6f1
Dhaval <dhaval.dotinfotech@gmail.com>
2021-07-08 18:33:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import Home from "./landing/home";
+
+function App() {
+  return <Home name="from Parent"></Home>;
+}
+
+export default App;
---
+++ src/landing/home.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "DOT Infotech",
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>Heading</h1>
+        <div>Home page</div>
+        <p>{this.state.name}</p>
+        <p>{this.props.name}</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
